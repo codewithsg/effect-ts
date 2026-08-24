bb89e5e4a3460730364ebf6472b35e2e009c89d8
Saragam Ghimire <codewithghimire@gmail.com>
2026-08-24 13:31:22 +0545

    run web on 8849

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,5 +1,13 @@
-import { Console, Effect } from "effect";
+import { Effect } from "effect";
 
-const program = Console.log("Hello, World from @app/web!");
+const program = Effect.sync(() => {
+    Deno.serve(
+        { 
+            port: 8849, 
+            onListen: () => console.log("running web on localhost:8849") 
+        }, 
+        () => new Response("Web running on 8849")
+    );
+});
 
 Effect.runSync(program);
